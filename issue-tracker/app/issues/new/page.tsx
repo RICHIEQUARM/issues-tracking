d7d68b3be495ab229ea43b18019955a5b8c8a704
richie <conduahrichard5@gmail.com>
2024-05-07 11:12:44 -0700

    creating the form for data

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,15 +1,36 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-4">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE className="mt-5" placeholder="Description"></SimpleMDE>
+    <form
+      className="max-w-xl space-y-4"
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE className="mt-5" placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Issues</Button>
-    </div>
+    </form>
   );
 };
 
